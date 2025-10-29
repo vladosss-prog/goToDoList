8fc14be318e19257a9210dda7f54880fd135fdaf
Kirill Ryazanov <zereksplay56740@gmail.com>
2025-10-29 14:33:23 +0600

    Now checkbox don't reset  after adding new task

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/dekrill/goToDoList
+
+go 1.25.3
+
+require github.com/google/uuid v1.6.0
--- main.go
+++ main.go
@@ -4,11 +4,14 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+
+	"github.com/google/uuid"
 )
 
 type ToDo struct {
 	Title string
 	Done  bool
+	ID    string
 }
 
 type ToDoPageData struct {
@@ -25,13 +28,26 @@ func handleMain(w http.ResponseWriter, r *http.Request) {
 	tmpl, err := template.ParseFiles("templates/index.html")
 	errHadnler(err)
 
-	if r.Method != http.MethodPost {
-		tmpl.Execute(w, nil)
+	if r.Method == http.MethodPost {
+		err := r.ParseForm()
+		errHadnler(err)
+
+		id := uuid.New()
+
+		for i := range data.Todos {
+			if r.Form.Has(data.Todos[i].ID) {
+				data.Todos[i].Done = true
+			} else {
+				data.Todos[i].Done = false
+			}
+		}
+
+		taskAppend := []ToDo{{r.FormValue("task"), false, id.String()}}
+		data.Todos = append(data.Todos, taskAppend...)
+
+		tmpl.Execute(w, data)
 		return
 	}
-
-	taskAppend := []ToDo{{r.FormValue("task"), false}}
-	data.Todos = append(data.Todos, taskAppend...)
 
 	tmpl.Execute(w, data)
 
